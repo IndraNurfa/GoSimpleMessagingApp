e7ebfc89d9635244d70b658983edc406ed3dd937
Indra Nurfa <ahmadindran@gmail.com>
2025-04-04 21:55:26 +0700

    feat:: add elk stack

--- app/repository/user_repository.go
+++ app/repository/user_repository.go
@@ -6,17 +6,27 @@ import (
 
 	"github.com/kooroshh/fiber-boostrap/app/models"
 	"github.com/kooroshh/fiber-boostrap/pkg/database"
+	"go.elastic.co/apm"
 )
 
 func InsertNewUser(ctx context.Context, user *models.User) error {
+	span, _ := apm.StartSpan(ctx, "InsertNewUser", "repository")
+	defer span.End()
+
 	return database.DB.Create(user).Error
 }
 
 func InsertNewUserSession(ctx context.Context, user *models.UserSession) error {
+	span, _ := apm.StartSpan(ctx, "InsertNewUserSession", "repository")
+	defer span.End()
+
 	return database.DB.Create(user).Error
 }
 
 func GetUserSessionByToken(ctx context.Context, token string) (models.UserSession, error) {
+	span, _ := apm.StartSpan(ctx, "GetUserSessionByToken", "repository")
+	defer span.End()
+	
 	var (
 		resp models.UserSession
 		err  error
@@ -26,6 +36,9 @@ func GetUserSessionByToken(ctx context.Context, token string) (models.UserSessio
 }
 
 func UpdateUserSessionToken(ctx context.Context, token, refreshToken string, tokenExpired time.Time) error {
+	span, _ := apm.StartSpan(ctx, "UpdateUserSessionToken", "repository")
+	defer span.End()
+	
 	return database.DB.Model(&models.UserSession{}).
 		Where("refresh_token = ?", refreshToken).
 		Updates(map[string]interface{}{
@@ -36,14 +49,21 @@ func UpdateUserSessionToken(ctx context.Context, token, refreshToken string, tok
 }
 
 func DeleteUserSessionByToken(ctx context.Context, token string) error {
+	span, _ := apm.StartSpan(ctx, "DeleteUserSessionByToken", "repository")
+	defer span.End()
+
 	return database.DB.Where("token = ?", token).Delete(&models.UserSession{}).Error
 }
 
 func GetUserByUsername(ctx context.Context, username string) (models.User, error) {
+	span, _ := apm.StartSpan(ctx, "GetUserByUsername", "repository")
+	defer span.End()
+
 	var (
 		resp models.User
 		err  error
 	)
+
 	err = database.DB.Where("username = ?", username).Last(&resp).Error
 	return resp, err
 }
